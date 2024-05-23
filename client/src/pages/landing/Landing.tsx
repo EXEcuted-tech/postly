0fd08be5b564c508ec161e525e90016fc0250f42
PonPonc <gybaez321@gmail.com>
2024-05-23 15:58:01 +0800

    [Added] Forgot Password

--- client/src/pages/landing/Landing.tsx
+++ client/src/pages/landing/Landing.tsx
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from "react";
 
 const Landing = () => {
-  return (
-    <div>Landing</div>
-  )
-}
+  return <div>Landing in the moon</div>;
+};
 
-export default Landing
+export default Landing;

1ab2dd77a149f0c999699bccd2cbd802ceecf4a2
FranzGwapo <franzcasimir.ondiano@gmail.com>
2024-05-24 02:16:54 +0800

    [Added] Forgot Page FE

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -8,9 +8,12 @@ import Landing from './pages/landing/Landing';
 import Home from './pages/home/Home';
 import Notifications from './pages/notification/Notifications';
 import Profile from './pages/profile/Profile';
+import ForgotPassword from "./pages/authentication/ForgotPassword";
+import OTPSend from "./pages/authentication/OTPSend";
 import Search from "./pages/search/Search";
 import Login from "./pages/authentication/Login"
 import Signup from "./pages/authentication/Signup"
+import PassChange from "./components/modal/PassChange";
 
 function App() {
   return (
@@ -23,7 +26,7 @@ function App() {
         <Route path="/profile/likes" element={<Profile/>}/>
         <Route path="/profile/dashboard" element={<Profile/>}/>
       </Route>
-      <Route element={<SignedOutLayout />} >
+      <Route element={<PassChange />} >
         <Route path="/" element={<Landing/>} />
       </Route>  
     </Routes>

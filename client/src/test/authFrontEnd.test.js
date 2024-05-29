b9c72c8f462e322d86779180fbcf3891f4ed8207
FranzGwapo <franzcasimir.ondiano@gmail.com>
2024-05-29 21:57:22 +0800

    Commit

--- client/src/test/authFrontEnd.test.js
+++ client/src/test/authFrontEnd.test.js
@@ -1,5 +1,5 @@
 const { Builder, By, until, Key } = require('selenium-webdriver');
-const {cleanTestData} = require('./commonFunction')
+const {cleanTestData, loginAccount} = require('./commonFunction')
 let driver;
 
 const {testConfig, registerAccount} = require('./commonFunction');
@@ -16,7 +16,7 @@ describe('Sign Up', () => {
     
     afterAll(async () => {
         await cleanTestData('19103523@usc.edu.ph');
-        //await driver.quit();
+        await driver.quit();
     });
 
     test('Should successfully register an account', async () => {
@@ -208,6 +208,9 @@ describe('Sign Up', () => {
         await driver.wait(until.elementLocated(By.className('leading-5')), 10000);
         const msg = await driver.findElement(By.className('leading-5')).getText();
         expect(msg).toBe('Password must be at least 8 characters long.'); 
+
+        let btnClose = await driver.findElement(By.name('signup-close'));
+        await btnClose.click();
     });
 });
 
@@ -215,42 +218,149 @@ describe('Sign Up', () => {
 //=====================
 //  LOG IN TEST CASES 
 //=====================
-// describe('Log In', () => {
-//     beforeAll(async () => {
-//         driver = await new Builder().forBrowser('chrome').build();
-//         await driver.get(`${URL}`);
-//         await registerAccount(driver); 
-//     });
+describe('Log In', () => {
+    beforeAll(async () => {
+        driver = await new Builder().forBrowser('chrome').build();
+        await driver.get(`${URL}`);
+        await registerAccount(driver); 
+    });
     
-//     afterAll(async () => {
-//         await cleanTestData('19103523@usc.edu.ph');
-//         await driver.quit();
-//     });
+    afterAll(async () => {
+        await cleanTestData('19103523@usc.edu.ph');
+        await driver.quit();
+    });
 
-//     test('Should successfully login to the application', async () => {
-//         await driver.wait(until.elementLocated(By.name('loginButton')), 10000);
-//         await driver.wait(until.elementIsVisible(driver.findElement(By.name('loginButton'))), 10000);
-//         await driver.wait(until.elementIsEnabled(driver.findElement(By.name('loginButton'))), 10000);
-//         await driver.wait(until.elementIsVisible(driver.findElement(By.name('loginButton'))), 10000);
-//         await driver.wait(until.elementIsEnabled(driver.findElement(By.name('loginButton'))), 10000);
-//         await driver.wait(until.elementLocated(By.name('loginButton')), 10000);
-//         await driver.wait(until.elementIsVisible(driver.findElement(By.name('loginButton'))), 10000);
-//         await driver.wait(until.elementIsEnabled(driver.findElement(By.name('loginButton'))), 10000);
-//         await driver.wait(until.elementIsVisible(driver.findElement(By.name('loginButton'))), 10000);
-//         await driver.wait(until.elementIsEnabled(driver.findElement(By.name('loginButton'))), 10000);
+    test('Should successfully login to the application', async () => {
+        await driver.wait(until.elementLocated(By.name('loginButton')), 10000);
+        await driver.wait(until.elementIsVisible(driver.findElement(By.name('loginButton'))), 10000);
+        await driver.wait(until.elementIsEnabled(driver.findElement(By.name('loginButton'))), 10000);
+        await driver.wait(until.elementIsVisible(driver.findElement(By.name('loginButton'))), 10000);
+        await driver.wait(until.elementIsEnabled(driver.findElement(By.name('loginButton'))), 10000);
+        await driver.wait(until.elementLocated(By.name('loginButton')), 10000);
+        await driver.wait(until.elementIsVisible(driver.findElement(By.name('loginButton'))), 10000);
+        await driver.wait(until.elementIsEnabled(driver.findElement(By.name('loginButton'))), 10000);
+        await driver.wait(until.elementIsVisible(driver.findElement(By.name('loginButton'))), 10000);
+        await driver.wait(until.elementIsEnabled(driver.findElement(By.name('loginButton'))), 10000);
 
-//         let btn = await driver.findElement(By.name('loginButton'));
-//         await btn.click();
+        let btn = await driver.findElement(By.name('loginButton'));
+        await btn.click();
 
-//         await driver.findElement(By.name("un_email")).sendKeys("katteu_cutie");
-//         await driver.findElement(By.name("password")).sendKeys("password");
+        await driver.findElement(By.name("un_email")).sendKeys("katteu_cutie");
+        await driver.findElement(By.name("password")).sendKeys("password");
 
-//         let btnSignIn = await driver.findElement(By.name('login-btn'));
-//         await btnSignIn.click();
+        let btnSignIn = await driver.findElement(By.name('login-btn'));
+        await btnSignIn.click();
 
-//         await driver.wait(until.elementLocated(By.className('bg-[#F3F5F7]')), 10000);
-//         const element = await driver.findElement(By.className('bg-[#F3F5F7]'));
-//         const classes = await element.getAttribute('class');
-//         expect(classes).toContain('bg-[#F3F5F7]');        
-//     });
-// });
+        await driver.wait(until.elementLocated(By.className('bg-[#F3F5F7]')), 10000);
+        const element = await driver.findElement(By.className('bg-[#F3F5F7]'));
+        const classes = await element.getAttribute('class');
+        expect(classes).toContain('bg-[#F3F5F7]');        
+    });
+
+    test('Displays error notification when username is not inputted', async () => {
+        await driver.navigate().back();
+
+        await driver.wait(until.elementLocated(By.name('loginButton')), 10000);
+        await driver.wait(until.elementIsVisible(driver.findElement(By.name('loginButton'))), 10000);
+        await driver.wait(until.elementIsEnabled(driver.findElement(By.name('loginButton'))), 10000);
+        await driver.wait(until.elementIsVisible(driver.findElement(By.name('loginButton'))), 10000);
+        await driver.wait(until.elementIsEnabled(driver.findElement(By.name('loginButton'))), 10000);
+        await driver.wait(until.elementLocated(By.name('loginButton')), 10000);
+        await driver.wait(until.elementIsVisible(driver.findElement(By.name('loginButton'))), 10000);
+        await driver.wait(until.elementIsEnabled(driver.findElement(By.name('loginButton'))), 10000);
+        await driver.wait(until.elementIsVisible(driver.findElement(By.name('loginButton'))), 10000);
+        await driver.wait(until.elementIsEnabled(driver.findElement(By.name('loginButton'))), 10000);
+
+        let btn = await driver.findElement(By.name('loginButton'));
+        await btn.click();
+
+        await driver.findElement(By.name("un_email")).sendKeys("");
+        await driver.findElement(By.name("password")).sendKeys("password");
+
+        let btnSignIn = await driver.findElement(By.name('login-btn'));
+        await btnSignIn.click();
+
+        await driver.wait(until.elementLocated(By.className('leading-5')), 10000);
+        const msg = await driver.findElement(By.className('leading-5')).getText();
+        expect(msg).toBe('Username or email is required!');       
+    });
+
+    test('Displays error notification when password is not inputted', async () => {
+        await driver.findElement(By.name("un_email")).sendKeys(Key.CONTROL, 'a', Key.DELETE);
+        await driver.findElement(By.name("password")).sendKeys(Key.CONTROL, 'a', Key.DELETE);
+
+        await driver.findElement(By.name("un_email")).sendKeys("katteu_cutie");
+        await driver.findElement(By.name("password")).sendKeys("");
+
+        let btnSignIn = await driver.findElement(By.name('login-btn'));
+        await btnSignIn.click();
+
+        await driver.wait(until.elementLocated(By.className('leading-5')), 10000);
+        const msg = await driver.findElement(By.className('leading-5')).getText();
+        expect(msg).toBe('Password is required!');       
+    });
+
+    test('Displays error notification when credentials are invalid', async () => {
+        await driver.findElement(By.name("un_email")).sendKeys(Key.CONTROL, 'a', Key.DELETE);
+        await driver.findElement(By.name("password")).sendKeys(Key.CONTROL, 'a', Key.DELETE);
+
+        await driver.findElement(By.name("un_email")).sendKeys("19103523@usc.edu.ph");
+        await driver.findElement(By.name("password")).sendKeys("passwieeerd");
+
+        let btnSignIn = await driver.findElement(By.name('login-btn'));
+        await btnSignIn.click();
+
+        await driver.wait(until.elementLocated(By.className('leading-6')), 1000);
+        const title = await driver.findElement(By.className('leading-6')).getText();
+        expect(title).toBe('Error!');       
+    });
+
+    test('Displays error notification when account does not exist', async () => {
+        await driver.findElement(By.name("un_email")).sendKeys(Key.CONTROL, 'a', Key.DELETE);
+        await driver.findElement(By.name("password")).sendKeys(Key.CONTROL, 'a', Key.DELETE);
+
+        await driver.findElement(By.name("un_email")).sendKeys("non-existent@gmail.com");
+        await driver.findElement(By.name("password")).sendKeys("passwirdzz");
+
+        let btnSignIn = await driver.findElement(By.name('login-btn'));
+        await btnSignIn.click();
+
+        await driver.wait(until.elementLocated(By.className('leading-5')), 10000);
+        const msg = await driver.findElement(By.className('leading-5')).getText();
+        expect(msg).toBe('Account does not exist!');
+        
+        let btnClose = await driver.findElement(By.name('login-close'));
+        await btnClose.click();
+    });
+});
+
+//======================
+//  LOG OUT TEST CASE 
+//======================
+describe('Log Out', () => {
+
+    beforeAll(async () => {
+        jest.setTimeout(10000);
+        driver = await new Builder().forBrowser('chrome').build();
+        await driver.get(`${URL}`);
+        await registerAccount(driver); 
+        await loginAccount(driver);
+    });
+    
+    afterAll(async () => {
+        await cleanTestData('19103523@usc.edu.ph');
+        await driver.quit();
+    });
+
+    test('Should successfully log out the application', async () => {
+        let btn = await driver.findElement(By.id('logout'));
+        await btn.click();
+
+        let btnLogout = await driver.findElement(By.name('logout-btn'));
+        await btnLogout.click();
+        await driver.sleep(1000);
+
+        const currentUrl = await driver.getCurrentUrl();
+        expect(currentUrl).toBe(`${URL}/`); 
+    });
+});
